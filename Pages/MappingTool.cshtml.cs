0002b1ad872637d59e90aee0c6f285fae1be0917
Mike Perholtz <orders@slopesidesystems.com>
2018-01-24 16:43:27 -0800

    Got map to show up but it slides under the top menu and disappears when trying to navigate back to page.

--- Pages/MappingTool.cshtml.cs
+++ Pages/MappingTool.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace commute_tool.Pages
 {
-    public class AboutModel : PageModel
+    public class MappingToolModel : PageModel
     {
         public string Message { get; set; }
 
--- Pages/QuickStart.cshtml.cs
+++ Pages/QuickStart.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CommuteTool.Pages
+namespace commute_tool.Pages
 {
     public class QuickStartModel : PageModel
     {
